9ee06f97e7317a7d71c9c1c4fbfe75bfa19b73d2
AshiikoAR <134614147+AshiikoAR@users.noreply.github.com>
2024-02-08 11:43:23 +0000

    modified:   front/src/app/task-create/task-create.component.html 	modified:   front/src/app/task-create/task-create.component.ts 	modified:   front/src/app/task-list/task-list.component.html 	modified:   front/src/app/task-list/task-list.component.ts

--- front/src/app/task-create/task-create.component.ts
+++ front/src/app/task-create/task-create.component.ts
@@ -11,18 +11,13 @@ export class TaskCreateComponent {
   constructor(private taskService: TaskService) {}
 
   createTask(title: string, description: string): void {
-    const task: Task = {
-      title, description,
-      id: 0
-    }; // Crée un objet Task avec les propriétés title et description
+    const task: Task = {title, description, id: 0}; // Création objet "Task" contenant titre + description
     this.taskService.createTask(task).subscribe(
       () => {
         console.log('Tâche créée avec succès !');
-        // Ajoutez ici toute autre logique nécessaire après la création de la tâche
       },
       (error) => {
         console.error('Erreur lors de la création de la tâche :', error);
-        // Ajoutez ici toute logique de gestion des erreurs
       }
     );
   }
--- front/src/app/task-list/task-list.component.ts
+++ front/src/app/task-list/task-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
+import { Task } from '../task';
 
 @Component({
   selector: 'app-task-list',
-  standalone: true,
-  imports: [],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskService.getTasks().subscribe(
+      (tasks) => {
+        this.tasks = tasks;
+        console.log('Liste des tâches récupérée avec succès !');
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération de la liste des tâches :', error);
+      }
+    );
+  }
 }
+
